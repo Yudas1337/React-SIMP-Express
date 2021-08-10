211e2392d2f18936bc34ca500789b8a42223a0ad
Yudas <yudasmalabi@gmail.com>
2021-08-10 16:34:14 +0700

    add headers token and authorization

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ hbs.localsAsTemplateData(app)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, X-Token-Auth, Authorization')
     next()
   });
   
